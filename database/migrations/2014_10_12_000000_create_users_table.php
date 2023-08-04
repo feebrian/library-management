5b4018faa7aced5923d4947cc8c1b2ec1cd6874e
feebrian <adamaditama13@gmail.com>
2023-08-04 10:03:11 +0700

    update user migration
    create new column:
    - status
    - user_permission
    - type

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status');
+            $table->string('user_permission');
+            $table->tinyInteger('type')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
